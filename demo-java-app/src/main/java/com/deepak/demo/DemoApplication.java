aaf8bf572c1d32a32bc30a301de3133fc366ed95
Louison SILLON <sillon01@protonmail.com>
2026-03-20 12:13:30 +0100

    modify to v3

--- demo-java-app/src/main/java/com/deepak/demo/DemoApplication.java
+++ demo-java-app/src/main/java/com/deepak/demo/DemoApplication.java
@@ -1,9 +1,10 @@
 package com.deepak.demo;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
@@ -30,7 +31,7 @@ public class DemoApplication {
         logger.info("INFO level log message");
         logger.debug("DEBUG level log message");
         logger.error("ERROR level log message");
-        model.addAttribute("title", "This is a Spring Boot demo app version v2.0");
+        model.addAttribute("title", "This is a Spring Boot demo app version v3.0");
         model.addAttribute("msg", "Implemented using DevOps.");
         return "index";
     }
